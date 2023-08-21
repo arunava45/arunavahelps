7a6a98446badf3833303b1514ea73624cb98e55a
arunava45 <bagal.arunava@gmail.com>
2023-08-21 15:27:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const imageLinks = document.querySelectorAll('.image-link');
 
+    imageLinks.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const largeImageSrc = this.getAttribute('href');
+            const lightbox = document.createElement('div');
+            lightbox.className = 'lightbox';
+            const image = document.createElement('img');
+            image.src = largeImageSrc;
+            lightbox.appendChild(image);
+            document.body.appendChild(lightbox);
+
+            lightbox.addEventListener('click', function () {
+                document.body.removeChild(lightbox);
+            });
+        });
+    });
+});
